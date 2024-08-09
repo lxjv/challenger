d32780225b3f321bf580aa404d63dc33d78e45be
Laker Turner <la@laker.gay>
2024-08-09 10:46:53 +0100

    okay its morbing time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://new.laker.tech",
+    integrations: [preact(), mdx()],
+    image: {
+        domains: ["cdn.laker.tech"],
+    }
+});
---
+++ src/content/blog/first.md
@@ -0,0 +1,10 @@
+---
+title: "hello"
+description: "test"
+pubDate: 2024-05-12
+author: "Laker Turner"
+tags: ["meta"]
+image: { url: "https://cdn.laker.tech/images/welcome.gif", alt: "welcome to the club" }
+---
+
+why
