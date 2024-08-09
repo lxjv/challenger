2743d61e3323371c95343a030ab56fa4454dcbbb
Laker Turner <la@laker.gay>
2024-08-09 21:06:40 +0100

    okay lets try vercel

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,20 @@
 // Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
     type: "content",
     schema: z.object({
+        draft: z.boolean().default(true),
         title: z.string(),
         pubDate: z.date(),
-        description: z.string(),
-        author: z.string(),
+        description: z.string().default("no description"),
+        relatedPosts: z.array(reference('blog')).optional(),
+        pinned: z.boolean().default(false),
+        author: z.string().default("Laker Turner"),
         image: z.object({
             url: z.string(),
             alt: z.string(),
-        }),
+        }).optional(),
         tags: z.array(z.string()),
     }),
 });
