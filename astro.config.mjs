f4cee3ab18629b86fe5fa50a5ce47f34a68da0a9
Laker Turner <la@laker.gay>
2024-08-10 11:53:33 +0100

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 
-import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://new.laker.tech",
-    integrations: [preact(), mdx()],
-    image: {
-        domains: ["cdn.laker.tech"],
-    }
+  site: "https://new.laker.tech",
+  integrations: [icon()],
+  image: {
+    domains: ["cdn.laker.tech"]
+  }
 });
