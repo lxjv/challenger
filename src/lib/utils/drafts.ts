46cb91b005e9070681bed6cbe751f82add6c7754
Laker Turner <la@laker.gay>
2024-08-12 12:24:37 +0100

    aeh

--- src/lib/utils/drafts.ts
+++ src/lib/utils/drafts.ts
@@ -22,6 +22,23 @@ export const getPublishedAndSortedPosts = (
             }
         });
 };
+export const getPublishedAndSortedNotes = (allPosts: CollectionEntry<"notes">[]) => {
+    return allPosts
+        .filter((post) => {
+            if (import.meta.env.PROD) {
+                return !post.data.draft;
+            }
+
+            return siteConfig.devMode.showDraftPages ? true : !post.data.draft;
+        })
+        .sort((post1, post2) => {
+            if (isDateBefore(post1.data.pubDate, post2.data.pubDate)) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+};
 
 // helper function to check if date1 is before date2
 export const isDateBefore = (date1: Date, date2: Date) =>
