de6cb66af8d99c432cb34e1d7bd8754ae0125788
Laker Turner <la@laker.gay>
2024-08-13 19:01:56 +0100

    fuck

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://new.laker.tech",
-  image: {
-    domains: ["cdn.laker.tech"]
-  },
-  redirects: {
-    '/feed.xml': '/blog/feed.rss'
-  }
+    site: "https://laker.tech",
+    image: {
+        domains: ["cdn.laker.tech"],
+    },
+    redirects: {
+        "/feed.xml": "/blog/feed.rss",
+    },
 });
