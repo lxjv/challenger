5b74388c2cc959c51e3e081ee57e41830db9091a
Laker Turner <la@laker.gay>
2024-08-16 18:44:46 +0100

    a3

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly LFM_API_KEY: string;
+    readonly LFM_USER: string;
+    // more env variables...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
--- src/lib/lfm/latestTrack.ts
+++ src/lib/lfm/latestTrack.ts
@@ -1,3 +1,9 @@
-import { Client } from "@lastfm-ts/wrapper";
+const apiURL = "https://ws.audioscrobbler.com/2.0/";
 
-const lfm = new Client(import.meta.LFM_API_KEY);
+export default function getLatestTrack() {
+    const recentTracks = fetch(
+        `${apiURL}?method=user.getrecenttracks&user=${import.meta.env.LFM_USER}&api_key=${import.meta.env.LFM_API_KEY}&format=json`,
+    );
+
+    console.log(recentTracks);
+}
