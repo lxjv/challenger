9ce59acf8aa6e2fffd010509c452ec387acb407b
Laker Turner <la@laker.gay>
2024-08-22 01:18:57 +0100

    unbroke notes rss feed and added a new node

---
+++ src/content/notes/first.md
@@ -0,0 +1,4 @@
+---
+pubDate: 2024-08-22
+---
+This should work with Echofeed! hello! 
--- src/pages/notes/feed.rss.js
+++ src/pages/notes/feed.rss.js
@@ -1,6 +1,5 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { siteConfig } from "@lib/siteConfig";
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
 const parser = new MarkdownIt();
@@ -9,8 +8,8 @@ export async function GET(context) {
     const posts = await getCollection("notes");
 
     return rss({
-        title: siteConfig.meta.title,
-        description: siteConfig.meta.description,
+        title: "laNotes",
+        description: "Notes, by Laker Turner",
         site: context.site,
         items: posts.map((post) => ({
             pubDate: post.data.pubDate,
@@ -20,6 +19,6 @@ export async function GET(context) {
             }),
             ...post.data,
         })),
-        customData: `<language>en-us</language>`,
+        customData: `<language>en-gb</language>`,
     });
 }
