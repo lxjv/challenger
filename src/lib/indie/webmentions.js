51619f36438f5e63e18ef289e01e493db00d5248
Laker Turner <la@laker.gay>
2024-08-27 21:14:42 +0100

    aeh

--- src/lib/indie/webmentions.js
+++ src/lib/indie/webmentions.js
@@ -39,7 +39,7 @@ function writeWebMention(webmention) {
         .replace(`https://${DOMAIN}/`, '')
         .replace(/\/$/, '')
         .replace('/', '--');
-    const filename = `./data/webmentions/${slug || 'home'}.json`;
+    const filename = `./.cache/webmentions/${slug || 'home'}.json`;
 
     // Create the file if it doesn't exist
     if (!fs.existsSync(filename)) {
