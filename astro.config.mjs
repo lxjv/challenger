f10ff5f0e7c85e66bf822ec2186dc3bd7be702c1
Laker Turner <la@laker.gay>
2024-09-09 22:13:22 +0100

    ae

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://laker.tech",
-    image: {
-        domains: ["cdn.laker.tech"],
-    },
-    integrations: [mdx(), preact()],
-    prefetch: {
-        prefetchAll: true
-    }
+  site: "https://laker.tech",
+
+  image: {
+      domains: ["cdn.laker.tech"],
+  },
+
+  integrations: [mdx(), preact()],
+
+  prefetch: {
+      prefetchAll: true
+  },
+
+  output: "static",
+  adapter: vercel()
 });
