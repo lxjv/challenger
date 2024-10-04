25dda42bf23f2a08fef609aa189c055108fb6692
Laker Turner <la@laker.gay>
2024-10-04 13:00:39 +0100

    fucking please

--- src/lib/filters/arrays.ts
+++ src/lib/filters/arrays.ts
@@ -2,6 +2,6 @@ export function getRandomItem(arr: Array<string>) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-export function getFirst(arr: Array) {
+export function getFirst(arr: Array<any>) {
     return arr[0];
 }
